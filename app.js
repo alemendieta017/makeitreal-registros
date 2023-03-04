f1c6da68484ad027a2758b7a340bd0ad5b01a8fb
alemendieta017 <amendieta017@gmail.com>
2023-03-04 19:28:29 -0300

    Add session cookie

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+var session = require('express-session')
 const { engine } = require('express-handlebars')
 const routes = require('./routes')
 
@@ -7,7 +9,13 @@ const routes = require('./routes')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, function (err) {
+  if (err) {
+    throw err
+  } else {
+    console.log('[DB] Database connected succesfully')
+  }
+})
 
 //views
 app.engine('hbs', engine({ extname: '.hbs' }))
@@ -16,6 +24,13 @@ app.set('views', './views')
 
 //middlewares
 
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- routes.js
+++ routes.js
@@ -1,10 +1,22 @@
 const { Router } = require('express')
+const bcrypt = require('bcrypt')
 const router = Router()
 const User = require('./models/user')
+const authMiddleware = require('./middlewares/auth')
 
-router.get('/', async function (req, res, next) {
+router.get('/', authMiddleware, async function (req, res, next) {
   const users = await User.find({}).lean()
   res.render('main', { users })
+})
+
+router.get('/users', async function (req, res, next) {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send()
+  }
 })
 
 router.get('/register', function (req, res, next) {
@@ -14,11 +26,28 @@ router.get('/register', function (req, res, next) {
 router.post('/register', async function (req, res, next) {
   try {
     const newUser = new User(req.body)
+    newUser.password = await bcrypt.hash(newUser.password, 10)
     await newUser.save()
     console.log(newUser)
     res.redirect('/')
   } catch (e) {
     console.log(e)
+    res.redirect('/')
+  }
+})
+
+router.get('/login', function (req, res, next) {
+  res.render('login')
+})
+
+router.post('/login', async function (req, res, next) {
+  const payload = req.body
+  const user = await User.findOne({ email: payload.email })
+  const passwordIsOk = await bcrypt.compare(payload.password, user.password)
+  if (!user || !passwordIsOk) {
+    res.status(404).send('Unauthorized')
+  } else {
+    req.session.userId = user._id
     res.redirect('/')
   }
 })
